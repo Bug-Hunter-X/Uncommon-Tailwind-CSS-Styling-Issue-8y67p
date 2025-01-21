187b29590bc3e4ac79050af4a410998db958c0be
BugHunter-X <askzenai@gmail.com>
2025-01-21 21:40:07 +0100

    Add SomeComponentSolution.js file

---
+++ SomeComponentSolution.js
@@ -0,0 +1,9 @@
+```javascript
+// SomeComponentSolution.js
+<div className="bg-gray-100 p-4">
+  <h2 className="text-xl font-bold mb-2">Some Component</h2>
+  <p>This component now renders correctly!</p>
+  <div className='relative'>   {/* Adding a relative div to fix the issue*/}
+  </div>
+</div>
+```
