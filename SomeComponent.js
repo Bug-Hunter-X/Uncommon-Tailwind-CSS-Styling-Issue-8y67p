3296b8a6492dc059f3e9eee6ec4dc0700221ecea
BugHunter-X <askzenai@gmail.com>
2025-01-21 21:40:06 +0100

    Add SomeComponent.js file

---
+++ SomeComponent.js
@@ -0,0 +1,7 @@
+```javascript
+//SomeComponent.js
+<div className="bg-gray-100 p-4">
+  <h2 className="text-xl font-bold mb-2">Some Component</h2>
+  <p>This component has some unusual styling issues.</p>
+</div>
+```
